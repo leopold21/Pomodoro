55dc69158b1ae64c38d95062f6c81ea33b5f1585
Léo-Paul <marie2328@unicaen.fr>
2024-09-23 14:36:27 +0200

    Mise en place du chrnonomètre avec une valeur pré-rentré. Début du design de la page.

---
+++ script.js
@@ -0,0 +1,69 @@
+let timerElement = document.getElementById("timer")
+let runElement = document.getElementById("boolRun")
+let stopElement = document.getElementById("boolStop")
+let pauseElement = document.getElementById("boolPause")
+// let startAgainElement = document.getElementById("boolStartAgain")
+
+let time = 25
+
+let timeSecondes = 0
+let timeMinutes = time
+
+let timer
+
+let running = false
+
+stopElement.style.display = "none"
+pauseElement.style.display = "none"
+//startAgainElement.style.display = "none"
+
+function decreaseTime(){
+    if(timeSecondes <= 0){
+        timeMinutes--
+        timeSecondes = 59
+    }else{
+        timeSecondes--
+    }
+    if(timeSecondes < 10){
+        timerElement.textContent = (timeMinutes + ":0" + timeSecondes)
+    }else{
+        timerElement.textContent = (timeMinutes + ":" + timeSecondes)
+    }
+}
+
+runElement.addEventListener("click", ()=>{
+    running = true
+    
+    stopElement.style.display = "block"
+    runElement.style.display = "none"
+
+    decreaseTime()
+    timer = setInterval(decreaseTime, 1000)
+
+    pauseElement.style.display = "block"
+})
+
+pauseElement.addEventListener("click", ()=>{
+    running = false
+    
+    pauseElement.style.display = "none"
+    runElement.style.display = "block"
+    clearInterval(timer)
+})
+
+
+stopElement.addEventListener("click", ()=>{
+    running = false
+
+    timeSecondes = 0
+    timeMinutes = time
+
+    timerElement.textContent = time + ":00"
+    
+    pauseElement.style.display = "none"
+    stopElement.style.display = "none"
+    runElement.style.display = "block"
+
+    clearInterval(timer)
+})
+
